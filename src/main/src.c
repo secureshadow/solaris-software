ca2b50591c025b3c6c30d213c2db4ce45a68b2e5
Solaris Team <solaris-dev@company.com>
2024-10-06 08:08:35 +0000

    New software repository.

--- src/main/src.c
+++ src/main/src.c
@@ -2,24 +2,44 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+// #include "pressure_sensor.h" 
 
-#define LED_PIN GPIO_NUM_2  // Change this to your LED pin number
+#define LED_PIN GPIO_NUM_2  
+
+/*  
+    TODO - Create FreeRTOS task that uses the pressure_sensor.h declared functions
+    OR - you can decalre the function in pressure_sensor.c and use it directly in main()
+
+    void get_pressure_data(){
+
+        #Declare variables here
+
+        while(1){
+            pressure_sensor_init();
+            pressure_sensor_read();
+        }
+
+    }
+*/
 
 void blink_task(void *pvParameter) {
-    // Configure the GPIO pin
+
     gpio_reset_pin(LED_PIN);
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 
     while (1) {
-        gpio_set_level(LED_PIN, 1);  // Turn the LED on
-        vTaskDelay(1000 / portTICK_PERIOD_MS);  // Delay for 1 second
-        gpio_set_level(LED_PIN, 0);  // Turn the LED off
-        vTaskDelay(1000 / portTICK_PERIOD_MS);  // Delay for 1 second
+        gpio_set_level(LED_PIN, 1);  
+        vTaskDelay(1000 / portTICK_PERIOD_MS);  
+        gpio_set_level(LED_PIN, 0); 
+        vTaskDelay(1000 / portTICK_PERIOD_MS);  
     }
 }
 
 void app_main(void) {
-    // Create the blink task
+
     xTaskCreate(blink_task, "blink_task", 1024, NULL, 5, NULL);
+
+    // Call the pressure sensor task here:
+    // xTaskCreate(get_pressure_data, ...)
+
 }
-//comentario
