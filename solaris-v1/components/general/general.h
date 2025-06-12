0429a7c67de4117df9ea9205d0d295560fd3131a
David Rodríguez <david.rodriguez.dagas@alumnado.uvigo.gal>
2025-06-12 18:38:14 +0000

    Structure adjust for setup()

--- solaris-v1/components/general/general.h
+++ solaris-v1/components/general/general.h
@@ -4,9 +4,13 @@
 #include "esp_err.h"
 #include "macros.h"  // Para la definición de data_t
 
-// Inicializa los sensores IMU y Barómetro utilizando los structs pasados como parámetros.
+// Inicializa la comunicación con IMU y barómetro utilizando los structs pasados como parámetros.
 // Devuelve ESP_OK si la inicialización es exitosa, o un código de error en caso contrario.
 esp_err_t init_common_sensors(data_t *icm, data_t *baro);
+
+// Realiza las configuraciones específicas de cada sensor
+// Despertart sensores, cambios de modo...
+esp_err_t configure_common_sensors(data_t *icm, data_t *baro);
 
 // Lee y muestra las mediciones del barómetro.
 // Se recibe el puntero al struct del barómetro.
