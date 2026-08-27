b014a1b8d3c38f0f38cd7e1b293b7142f74b3eae
Oscar Barja <oscar.barja@alumnado.uvigo.gal>
2026-08-27 23:52:47 +0000

    minor changes

--- solaris-v2/main/main.c
+++ solaris-v2/main/main.c
@@ -1,6 +1,5 @@
 #include "spp/services/fsm/fsm.h"
 #include "spp/ports/hal/esp32/halEsp32.h"
-#include "spp/hal/time/time.h"
 
 void app_main(void)
 
@@ -10,27 +9,9 @@ void app_main(void)
     // Pass the HAL port to the FSM
     (void)FSM_init((void *)p_halPorts);
 
-
     // Call the Herarchical Finite State Machine
-    spp_int16_t i = 0;
-    spp_uint32_t t0 = 0;
-    spp_uint32_t t1 = 0;
-    spp_uint32_t dt[500] = {0};
-
-    FSM_tick();
-    FSM_tick();
-
-    while (i < 500)
+    while (1)
     {
-        t0 = SPP_HAL_TIME_getTimeUs();
         FSM_tick();
-        t1 = SPP_HAL_TIME_getTimeUs();
-        dt[i] = t1 - t0;
-        i++;
-    }
-
-    for (spp_int16_t j = 0; j < 500; j++)
-    {
-        printf("dt[%d] = %u\n", j, dt[j]);
     }
 }
